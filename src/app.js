85b90facca98addf0d519cba4eac70698ebd6ccd
Gaurav Singh Sirari <gauravsirari424@gmail.com>
2025-05-25 10:29:45 +0530

    exploring all request

--- src/app.js
+++ src/app.js
@@ -1,15 +1,51 @@
+// const express = require("express");
+
+// const app = express();
+
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello Hello ");
+// });
+
+// app.use("/test/23", (req, res) => {
+//   res.send("hello  Gaurav   singh sirari from the server");
+// });
+
+
+
+// // app.use("/", (req, res) => {
+// //   res.send("Nahi pata");
+// // });
+
+
+// app.listen(3000, () => {
+//   console.log("successfully listeing");
+// });
+
+
 const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("hello  Gaurav   singh sirari from the server");
+app.use("/user",(req,res)=>{
+  res.send("hahah aaya samaj atuje ");
+  
+})
+
+app.post("/user",(req,res)=>{
+  res.send("hello testing saved ")
 });
 
-app.use("/you", (req, res) => {
-  res.send("Ky yaar pad q nhi raha hai okkk ");
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Gaurav",lastName:"Sirari"})
 });
 
-app.listen(3000, () => {
-  console.log("successfully listeing");
+app.delete("/user",(req,res)=>{
+  res.send("Deleted successfully")
 });
+
+
+app.listen(3000,()=>{
+  console.log("Successfully kam kar rha hu mai");
+  
+})
