b36d1b0392626177f758f29ceea7a87e9d0fae1a
Gaurav Singh Sirari <gauravsirari424@gmail.com>
2025-05-26 13:46:11 +0530

    Learn about route and route handlers

--- src/app.js
+++ src/app.js
@@ -1,51 +1,45 @@
-// const express = require("express");
-
-// const app = express();
-
-
-// app.use("/test", (req, res) => {
-//   res.send("Hello Hello ");
-// });
-
-// app.use("/test/23", (req, res) => {
-//   res.send("hello  Gaurav   singh sirari from the server");
-// });
-
-
-
-// // app.use("/", (req, res) => {
-// //   res.send("Nahi pata");
-// // });
-
-
-// app.listen(3000, () => {
-//   console.log("successfully listeing");
-// });
-
-
 const express = require("express");
 
 const app = express();
-
-app.use("/user",(req,res)=>{
-  res.send("hahah aaya samaj atuje ");
   
-})
-
-app.post("/user",(req,res)=>{
-  res.send("hello testing saved ")
-});
-
-app.get("/user",(req,res)=>{
-  res.send({firstName:"Gaurav",lastName:"Sirari"})
-});
-
-app.delete("/user",(req,res)=>{
-  res.send("Deleted successfully")
-});
 
 
-app.listen(3000,()=>{
-  console.log("Successfully kam kar rha hu mai");
+app.use("/user",
+  [
+  (req,res,next)=>{
+ 
+
+  console.log("response 1 console");
+  res.send("reponse 1 ")
+  next();
+
+  
+},
+(req,res,next)=>{
+
+  console.log("response 2 console");
+  // res.send("response 2");
+  next();
+
+  
+},],
+(req,res,next)=>{
+  console.log("response 3 console");
+  
+  // res.send("response 3")
+  next();
+},
+(req,res,next)=>{
+  console.log("response 4 console");
+   res.send("respnse 4")
+
+  
+}
+
+);
+
+
+app.listen(7777,()=>{
+  console.log("Successfully working on port number 7777");
   
 })
